17db4fe08af2f101dfb377ac0a32409d0a7f13e9
JulioNunesDev <julionunes999@gmail.com>
2022-07-10 20:55:49 -0300

    update: rotas controllers

--- src/routes/controllers/product-controller.js
+++ src/routes/controllers/product-controller.js
@@ -1,6 +1,40 @@
 'use strict';
 const Product = require('../../models/Product')
 
+exports.get = (req, res, next)=>{
+    Product.find({active: true}, 'title price slug').then((produt)=>{
+        res.status(200).send(produt)
+    }).catch((err)=>{
+        res.status(201).send(err)
+    })
+    
+}
+
+exports.getBySlug = (req, res, next)=>{
+    Product.findOne({slug: req.params.slug ,active: true}, 'title price description slug tags').then((produt)=>{
+        res.status(200).send(produt)
+    }).catch((err)=>{
+        res.status(201).send(err)
+    })
+}
+
+exports.getById = (req, res, next)=>{
+    Product.findById(req.params.id).then((produt)=>{
+        res.status(200).send(produt)
+    }).catch((err)=>{
+        res.status(201).send(err)
+    })
+}
+
+
+exports.getByTag = (req, res, next)=>{
+    Product.find({tags: req.params.tag, active: true}, 'title price slug tags description' ).then((produt)=>{
+        res.status(200).send(produt)
+    }).catch((err)=>{
+        res.status(201).send(err)
+    })
+}
+
 exports.post = (req, res, next)=>{
     const produto = new Product(req.body)
     produto.save().then((produt)=>{
