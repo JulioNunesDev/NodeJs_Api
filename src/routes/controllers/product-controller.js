639f11f1890960569db26001a41c7a4eb84a0ea4
JulioNunesDev <julionunes999@gmail.com>
2022-07-12 18:29:20 -0300

    update: products controllers

--- src/routes/controllers/product-controller.js
+++ src/routes/controllers/product-controller.js
@@ -1,5 +1,6 @@
 'use strict';
 const Product = require('../../models/Product')
+const ValidatorProduct = require('../../validators/fluent-validator')
 
 exports.get = (req, res, next)=>{
     Product.find({active: true}, 'title price slug').then((produt)=>{
@@ -36,6 +37,15 @@ exports.getByTag = (req, res, next)=>{
 }
 
 exports.post = (req, res, next)=>{
+    const Validador = new ValidatorProduct()
+    Validador.hasMinLen(req.body.title, 3 ,'O titulo deve conter pelo menos 3 caracteres')
+
+    //os dados sao inválidos
+
+    if(!Validador.isValid()){
+        res.status(400).send(Validador.erros()).end()
+        return
+    }
     const produto = new Product(req.body)
     produto.save().then((produt)=>{
         res.status(201).send({message: 'Produto criado com sucesso!'})
@@ -45,9 +55,31 @@ exports.post = (req, res, next)=>{
 }
 
 exports.put = (req, res, next)=>{
-    res.status(200).send()
+    Product.findByIdAndUpdate(req.params.id, {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price
+      }
+    }).then((produt)=>{
+        res.status(200).send({message: 'Produto Atualizado com sucesso!'})
+    })
+    .catch((err)=>{
+        res.status(400).send({message: 'Falha ao Atualizar o produto!', data: err});
+    })
 }
 
 exports.delete = (req, res, next)=>{
-    res.status(200).send()
+    Product.findOneAndRemove(req.params.id, {
+        $set: {
+          title: req.body.title,
+          description: req.body.description,
+          price: req.body.price
+        }
+      }).then((produt)=>{
+          res.status(200).send({message: 'Produto Removido com sucesso!'})
+      })
+      .catch((err)=>{
+          res.status(400).send({message: 'Falha ao Remover o produto!', data: err});
+      })
 }
